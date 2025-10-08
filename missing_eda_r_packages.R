50e59d702fc9627dab481664f715cda47711af35
rserran <rserran1@gmail.com>
2025-10-07 20:06:42 -0400

    Update missing_eda_r_packages.R
    Add mcar_test, t-test for selected variables and correlation funnel.

--- missing_eda_r_packages.R
+++ missing_eda_r_packages.R
@@ -6,6 +6,7 @@ library(naniar)
 library(visdat)
 library(heatmaply)
 library(DataExplorer)
+library(rlang)
 
 # read Titanic dataset
 df <- read_csv('./titanic.csv')
@@ -18,6 +19,12 @@ skimr::skim(df)
 # plot_missing
 df |> 
      plot_missing()
+
+# verify if missing mechanism is MCAR
+mcar_test(df)
+
+# Since p value is < 0.05, then we reject the null hypothesis (data is MCAR) for 
+# the alternate hypothesis (data is not MCAR)
 
 # vis_miss()
 df |> 
@@ -61,9 +68,109 @@ df_title <- df |>
      ) |> 
      select(-name)
 
+# gg_miss_fct with survived
+df_title |> 
+     select(-cabin, -body, -boat, -home.dest) |> 
+     gg_miss_fct(
+          fct = survived
+     ) + 
+     labs(title = 'Missing values in Titanic by Passenger Survival')
+
+# gg_miss_fct with title
 df_title |> 
      select(-cabin, -body, -boat, -home.dest) |> 
      gg_miss_fct(
           fct = title
      ) + 
      labs(title = 'Missing values in Titanic by Passenger Title')
+
+# gg_miss_fct with sex
+df_title |> 
+     select(-cabin, -body, -boat, -home.dest) |> 
+     gg_miss_fct(
+          fct = sex
+     ) + 
+     labs(title = 'Missing values in Titanic by Passenger Gender')
+
+# t-test missingness impact function
+# Source: https://njtierney.github.io/talk-funfun-slc/#/so-eventually-i-wrote-a-function-for-my-missing-data-problem
+
+missingness_impact <- function(when_missing, is_different){
+     when_missing_index <- which(is.na(when_missing))
+     when_complete_index <- which(!is.na(when_missing))
+     
+     is_different_miss <- is_different[when_missing_index]
+     is_different_complete <- is_different[when_complete_index]
+     
+     result <- t.test(is_different_miss, y = is_different_complete)
+     
+     result
+     
+}
+
+# when_missing = df_title$age, is_different = df_title$survived
+missingness_impact(
+     when_missing = df_title$age, 
+     is_different = df_title$survived
+)
+
+# smae function with tidyverse syntax
+missingness_impact <- function(data, when_missing, is_different) {
+     when_missing <- enquo(when_missing)
+     is_different <- enquo(is_different)
+     
+     data_miss <- data %>% filter(is.na(!!when_missing))
+     data_complete <- data %>% filter(!is.na(!!when_missing))
+     
+     t.test(
+          data_miss %>% pull(!!is_different),
+          data_complete %>% pull(!!is_different)
+     )
+}
+
+df_title |> 
+     missingness_impact(
+          when_missing = age, 
+          is_different = survived
+     )
+
+df_title <- df_title |> 
+     add_label_missings(
+          age, 
+          fare, 
+          missing = 1, 
+          complete = 0) |> 
+     rename(age_missing = any_missing)
+
+df_title_imp <- df_title |> 
+     select(-survived, -cabin, -body, -boat, -home.dest, -age_missing) |> 
+     missRanger::missRanger(seed = 4354)
+
+summary(df_title$age)
+
+summary(df_title_imp$age)
+
+df_title_imp |> 
+     glimpse()
+
+# add survived and age_missing columns
+df_title_imp$age_missing <- df_title$age_missing
+df_title_imp$survived <- df_title$survived
+
+df_title_imp |> 
+     plot_correlation()
+
+# correlation funnel
+library(correlationfunnel)
+
+binarized_tbl <- df_title_imp |> 
+     binarize()
+
+binarized_tbl |> 
+     glimpse()
+
+corr_tbl <- binarized_tbl |>
+     correlate(survived__1)
+
+corr_tbl |> 
+     plot_correlation_funnel(interactive = TRUE)
